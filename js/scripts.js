d091357a9d16c89b2e0866cd200037aa28e5967b
Francisco Deppe <francisco.deppe@gmail.com>
2017-11-20 10:58:22 -0800

    updates to js files - can edit square size - add color next

--- js/scripts.js
+++ js/scripts.js
@@ -3,15 +3,13 @@ let hoverIn = e => {
   };
   
   let createSquares = () => {
-    main = document.getElementById("main");
-    main.innerHTML = "";
-  
-    let x = document.getElementById("my-input").value;
-  
-    x = x * x;
-  
+    main = document.getElementById("main");    
+    
+    squares = 480000 / 400
+
     let y = 0;
-    for (let i = 0; i < x; i++) {
+
+    for (let i = 0; i < squares; i++) {
       let block = document.createElement("div");
       block.style.width = "20px";
       block.style.height = "20px";
@@ -23,6 +21,31 @@ let hoverIn = e => {
       document.getElementById("main").appendChild(block);
     }
   };
+
+  createSquares();
+
+  let changeSquareSize = () => {
+    
+    let x = document.getElementById("my-input").value;
+    
+    squares = 480000 / (x * x)
+
+      main = document.getElementById("main");
+      main.innerHTML = "";
+    
+      let y = 0;
+      for (let i = 0; i < squares; i++) {
+        let block = document.createElement("div");
+        block.style.width = x + "px";
+        block.style.height = x + "px";
+        block.style.background = "grey";
+        block.classList.add("object");
+        block.setAttribute("id", y);
+        block.addEventListener("mouseover", hoverIn);
+        y++;
+        document.getElementById("main").appendChild(block);
+      }
+  }
   
   let reset = () => {
     var x = document.getElementById("main");
