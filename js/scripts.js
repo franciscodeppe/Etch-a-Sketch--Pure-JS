6254ccd80cc75a8240cc540c7f87c428a46282af
Francisco Deppe <francisco.deppe@gmail.com>
2017-11-20 12:04:59 -0800

    trying to get change color working

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,51 @@
-let hoverIn = e => {
-    e.target.style.background = "teal";
-  };
-  
-  let createSquares = () => {
-    main = document.getElementById("main");    
-    
+let color = "teal"
+
+let updateColor = (element) => {
+    let color = element.id;
+    console.log(color)
+    return color;
+}
+
+let hoverIn = (e) => {
+    console.log(color)
+    e.target.style.background = color;
+
+}; 
+
+
+let createSquares = () => {
+    main = document.getElementById("main");
+
     squares = 480000 / 400
 
     let y = 0;
 
     for (let i = 0; i < squares; i++) {
-      let block = document.createElement("div");
-      block.style.width = "20px";
-      block.style.height = "20px";
-      block.style.background = "grey";
-      block.classList.add("object");
-      block.setAttribute("id", y);
-      block.addEventListener("mouseover", hoverIn);
-      y++;
-      document.getElementById("main").appendChild(block);
+        let block = document.createElement("div");
+        block.style.width = "20px";
+        block.style.height = "20px";
+        block.style.background = "grey";
+        block.classList.add("object");
+        block.setAttribute("id", y);
+        block.addEventListener("mouseover", hoverIn);
+        y++;
+        document.getElementById("main").appendChild(block);
     }
-  };
+};
 
-  createSquares();
+createSquares();
 
-  let changeSquareSize = () => {
-    
+let changeSquareSize = () => {
+
     let x = document.getElementById("my-input").value;
-    
+
     squares = 480000 / (x * x)
 
-      main = document.getElementById("main");
-      main.innerHTML = "";
-    
-      let y = 0;
-      for (let i = 0; i < squares; i++) {
+    main = document.getElementById("main");
+    main.innerHTML = "";
+
+    let y = 0;
+    for (let i = 0; i < squares; i++) {
         let block = document.createElement("div");
         block.style.width = x + "px";
         block.style.height = x + "px";
@@ -44,15 +55,14 @@ let hoverIn = e => {
         block.addEventListener("mouseover", hoverIn);
         y++;
         document.getElementById("main").appendChild(block);
-      }
-  }
-  
-  let reset = () => {
+    }
+}
+
+let reset = () => {
     var x = document.getElementById("main");
     var y = x.getElementsByClassName("object");
     var i;
     for (i = 0; i < y.length; i++) {
-      y[i].style.backgroundColor = "grey";
+        y[i].style.backgroundColor = "grey";
     }
-  };
-  
+};
